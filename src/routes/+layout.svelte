f084d8272bc7a5a692070689abe87ea077538b2b
Brandon Lucas <38222767+thebrandonlucas@users.noreply.github.com>
2023-08-11 20:02:00 -0400

    fix: nav

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import '../app.css';
-	import Nav from '../features/Nav.svelte';
+	// import Nav from '../features/Nav.svelte';
 </script>
 
 <main class="flex flex-col mx-auto items-center">
