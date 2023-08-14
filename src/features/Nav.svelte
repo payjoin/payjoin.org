2866451bc609f28e86365efc07f6d69f3ad0c728
Brandon Lucas <38222767+thebrandonlucas@users.noreply.github.com>
2023-08-14 09:53:18 -0400

    feat: mobile friendly nav

--- src/features/Nav.svelte
+++ src/features/Nav.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
 	import { browser } from '$app/environment';
+	import { slide } from 'svelte/transition';
 	import Link from '../components/Link.svelte';
+	import Icon from '../components/Icon/Icon.svelte';
+	import { clickOutside } from '$lib/events';
 
 	// Change the background color of Nav when the user scrolls
 	function handleScroll(scrollY: number) {
@@ -11,27 +14,77 @@
 
 			if (scrollY > 0) {
 				nav?.classList.add('bg-black');
-			} else {
+			} else if (scrollY === 0 && !open) {
 				nav?.classList.remove('bg-black');
 			}
 		}
 	}
 
+	function handleOpen(event: MouseEvent) {
+		if (browser && y <= 0) {
+			const nav = document.querySelector('nav');
+			nav?.classList.add('bg-black');
+		}
+		open = true;
+		event.stopPropagation();
+	}
+
+	function handleClose() {
+		open = false;
+	}
+
 	let y = 0;
+	let open = false;
 	$: handleScroll(y);
 </script>
 
 <svelte:window bind:scrollY={y} />
 
-<nav class="fixed w-full flex justify-center transition-all duration-500 bg-opacity-90">
-	<ul class="flex justify-end p-4 gap-4 items-center text-xl">
-		<li><Link href="#">Why</Link></li>
-		<li><Link href="#">How to</Link></li>
-		<li><Link href="#">Demo</Link></li>
-		<li><Link href="#">Privacy</Link></li>
-		<li><Link href="#">Dev kit</Link></li>
-		<li><Link href="#">Join</Link></li>
-		<!-- TODO: light mode styling -->
-		<!-- <li><button on:click={toggleDarkMode}><Icon name="moon" /></button></li> -->
-	</ul>
+<nav
+	class="fixed w-full flex justify-between transition-all duration-500 bg-opacity-90 items-center p-4 z-10"
+>
+	<!-- Hamburger menu -->
+	{#if !open}
+		<div class="flex justify-between w-full md:hidden">
+			<h3 class="text-3xl text-white self-start">
+				<a href="/">Payjoin</a>
+			</h3>
+
+			<button class="flex items-center" on:click={handleOpen}>
+				<Icon class="flex h-6 text-white" name="hamburger" />
+			</button>
+		</div>
+	{:else}
+		<div
+			class="md:hidden flex items-center w-full"
+			in:slide|local
+			out:slide|local
+			use:clickOutside={handleClose}
+		>
+			<div class="flex flex-col items-center justify-center h-full gap-4 w-full">
+				<button on:click={handleClose}>
+					<Icon class="flex h-6 text-white" name="close" />
+				</button>
+				<h3 class="text-3xl text-white"><a href="/">Payjoin</a></h3>
+				<ul class="hidden justify-end items-center text-xl md:flex">
+					<li><Link href="#">Why</Link></li>
+					<li><Link href="#">How to</Link></li>
+					<li><Link href="#">Demo</Link></li>
+					<li><Link href="#">Privacy</Link></li>
+					<li><Link href="#">Dev kit</Link></li>
+					<li><Link href="#">Join</Link></li>
+					<!-- TODO: light mode styling -->
+					<!-- <li><button on:click={toggleDarkMode}><Icon name="moon" /></button></li> -->
+				</ul>
+				<ul class="flex flex-col gap-4 items-center text-2xl">
+					<li><Link href="#">Why</Link></li>
+					<li><Link href="#">How to</Link></li>
+					<li><Link href="#">Demo</Link></li>
+					<li><Link href="#">Privacy</Link></li>
+					<li><Link href="#">Dev kit</Link></li>
+					<li><Link href="#">Join</Link></li>
+				</ul>
+			</div>
+		</div>
+	{/if}
 </nav>
