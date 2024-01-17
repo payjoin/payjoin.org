a6c7316a71a101ed7644e788e7b233be2f1de6fe
DanGould <d@ngould.dev>
2024-01-17 01:21:29 -0500

    Clean up scale page

--- src/routes/scale/+page.svelte
+++ src/routes/scale/+page.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <svelte:head>
-	<title>Scaling</title>
+	<title>How Does Payjoin Scale Bitcoin?</title>
 </svelte:head>
 
 <section class="text-white mt-24 w-2/3 flex flex-col gap-4 text-xl">
@@ -12,8 +12,11 @@
 	<div class="flex flex-col gap-4">
 		<div>
 			The Bitcoin blockchain is limited by block size. Approximately every ten minutes a new block
-			is added to the chain. New blocks add 2MB of data on average [[1]](). Blocks are limited by
-			transaction weight, or how much of each type of data is included in a transaction.
+			is added to the chain. New blocks are limited to around 2MB of data <a
+				class="underline"
+				href="https://bitcoin.stackexchange.com/a/116350"
+				target="_blank">on average</a
+			>.
 		</div>
 		<div>
 			The simplest way to scale any database is batching, which is exactly what Payjoin does.
@@ -41,33 +44,39 @@
 			sure about who got paid how much.
 		</div>
 		<div>
-			The Payjoin using exclusively Pay-to-Taproot addresses (P2TR)
-			[here](https://mutinynet.com/tx/3c5436f1edf7d4c32a5ccf2448c1e963f52bb8a0fb6f8688d7e78a14e1cbe80b)
+			The Payjoin using exclusively Pay-to-Taproot addresses (P2TR) <a
+				class="underline"
+				href="https://mutinynet.com/tx/3c5436f1edf7d4c32a5ccf2448c1e963f52bb8a0fb6f8688d7e78a14e1cbe80b"
+				target="_blank">here</a
+			>
 			is 211.75 vB. An analogous P2TR payment
-			[here](https://mutinynet.com/tx/2c45dc6fef9feb32b9741cc3e6197eda94e1b0c45675e18818bfadce9fa94e20)
+			<a
+				class="underline"
+				href="https://mutinynet.com/tx/2c45dc6fef9feb32b9741cc3e6197eda94e1b0c45675e18818bfadce9fa94e20"
+				target="_blank">here</a
+			>
 			is 152.25 vB and P2TR consolidation
-			[here](https://mutinynet.com/tx/ef9263ed05c07f7ba933389eee7bfd62372e3dc4d1e697f96b7c66a215cc9b46)
-			is 168.5 vB, for a total of 320.75 vB. The separate payment and consolidation have to pay for
-			51% more block weight to be mined than the Payjoin. What other scaling solution achieves that
-			kind of savings?
+			<a
+				class="underline"
+				href="https://mutinynet.com/tx/ef9263ed05c07f7ba933389eee7bfd62372e3dc4d1e697f96b7c66a215cc9b46"
+				target="_blank">here</a
+			> is 168.5 vB, for a total of 320.75 vB. The separate payment and consolidation have to pay for
+			51% more block weight to be mined than the Payjoin. What other scaling solution achieves that kind
+			of savings?
 		</div>
 
 		<div class="flex flex-col gap-4">
 			<H2>Opportunistic Consolidation</H2>
 
 			<div>
-				Payjoin got its start as a way to make a sort of [coinjoin] from a payment. A receiver
-				combines their input with the sender's, effectively joining a [consolidation]() transaction
-				with a simple transfer. An observer looking at the payjoin is cannot tell it apart from a
-				simple transfer where all of the inputs come from the same entity.
+				Payjoin got its start as a way to make a sort of coinjoin from a payment. A receiver
+				combines their input with the sender's, effectively joining a <a
+					class="underline"
+					href="https://bitcoin.stackexchange.com/questions/103194/confused-about-utxo-management-and-consolidation"
+					target="_blank">consolidation</a
+				> transaction with a simple transfer. An observer looking at the payjoin is cannot tell it apart
+				from a simple transfer where all of the inputs come from the same entity.
 			</div>
-
-			<div>
-				<!-- < need visuals > -->
-				TODO: visuals
-			</div>
-
-			<div>not only does the total weight get shaved. wouldn't it be cool when wallet</div>
 		</div>
 		<div class="flex flex-col gap-4">
 			<H2>Transaction Cut-Through</H2>
@@ -77,9 +86,6 @@
 				lightning channels, forward funds to a different wallet, pay for goods and services, or
 				batch forward transactions with incoming funds without first taking them into a new UTXO.
 			</div>
-
-			<!-- https://chaincase.app/words/lightning-payjoin https://payjoin.substack.com/p/interactive-payment-batching-is-better -->
-			<!-- [1](https://bitcoin.stackexchange.com/a/116350)  {#1} -->
 		</div>
 	</div>
 </section>
