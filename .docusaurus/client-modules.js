33eef2393f5f6cbb3fe158ef4e60a488231e3f04
Brandon Lucas <38222767+thebrandonlucas@users.noreply.github.com>
2024-06-02 15:07:40 -0400

    respect .gitignore rules

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/username/src/payjoin/payjoin.org/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/username/src/payjoin/payjoin.org/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/username/src/payjoin/payjoin.org/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/username/src/payjoin/payjoin.org/src/css/custom.css"),
-];
