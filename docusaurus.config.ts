6f1fc73c7dc4d2bec96f88f796f1a37be4175c6a
Brandon Lucas <38222767+thebrandonlucas@users.noreply.github.com>
2024-06-02 14:53:34 -0400

    change 'Edit This Page' link

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -39,7 +39,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/payjoin/payjoin.org/issues/new",
         },
         blog: {
           showReadingTime: true,
