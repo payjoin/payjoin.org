04052c9c258e797c17a92abdfdea985a0d837a47
Brandon Lucas <38222767+thebrandonlucas@users.noreply.github.com>
2024-06-02 20:47:51 -0400

    remove svg comments

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -10,7 +10,6 @@ type FeatureItem = {
 const FeatureList: FeatureItem[] = [
   {
     title: "Preserve Privacy",
-    // Svg: require("@site/static/img/ngu.svg").default,
     description: (
       <>
        Payjoin preserves privacy by breaking the most common method used to spy on bitcoin users
@@ -20,7 +19,6 @@ const FeatureList: FeatureItem[] = [
   },
   {
     title: "Scale Bitcoin, lower fees",
-    // Svg: require("@site/static/img/bitcoin-moneybag.svg").default,
     description: (
       <>
        Payjoin can settle many transactions at once, allowing higher throughput, time savings, and lower fees
@@ -30,7 +28,6 @@ const FeatureList: FeatureItem[] = [
   },
   {
     title: "Augment Lightning",
-    // Svg: require("@site/static/img/superhero.svg").default,
     description: (
       <>
         Payjoin enables Lightning nodes to fund and open all their channels in one transaction while maintaining privacy
@@ -43,9 +40,6 @@ const FeatureList: FeatureItem[] = [
 function Feature({ title, description, link }: FeatureItem) {
   return (
     <div className="bg-secondary rounded-lg p-4 w-72 flex flex-col justify-between items-start">
-      {/* {Svg && <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>} */}
       <div className="text-left text-xl">
         <Heading as="h3">{title}</Heading>
         <p>{description}</p>
