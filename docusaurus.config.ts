5fa02aba2b072cb2d3fb74e69b689c194b6562ed
Brandon Lucas <blu@blu.cx>
2025-10-27 00:32:46 -0400

    fix: broken 'edit this page' link

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -63,14 +63,14 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/payjoin/payjoin.org",
+            "https://github.com/payjoin/payjoin.org/blob/master",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/payjoin/payjoin.org",
+            "https://github.com/payjoin/payjoin.org/blob/master",
           blogSidebarTitle: 'All posts',
           blogSidebarCount: 'ALL',
         },
